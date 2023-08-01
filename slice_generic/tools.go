caf98f67f813d2ca85abac6a2f20a4f54acc782c
陈瀚禧 <gz4z2b@163.com>
2023-08-01 22:00:26 +0800

    注释

--- slice_generic/tools.go
+++ slice_generic/tools.go
@@ -2,8 +2,8 @@
  * @Author: p_hanxichen
  * @Date: 2023-07-31 21:21:42
  * @LastEditors: p_hanxichen
- * @LastEditTime: 2023-07-31 22:18:23
- * @FilePath: /src/classthreehomework/slice_generic/tools.go
+ * @LastEditTime: 2023-08-01 21:59:31
+ * @FilePath: /go/src/slice/slice_generic/tools.go
  * @Description:
  *
  * Copyright (c) 2023 by gdtengnan, All Rights Reserved.
@@ -12,6 +12,7 @@ package slicegeneric
 
 import "fmt"
 
+// Del 删除元素
 func Del[T any](slice []T, index int) []T {
 	if len(slice) < index {
 		panic("超限")
